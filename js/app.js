522ef333bda0296bede9bc787aff026ac575a467
Niky <nicolo.chioato@gmail.com>
2021-08-04 23:22:55 +0200

    Cominciata mappatura carte

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+// @ts-check
 import * as mappa from './mappa.js';
 import * as barrage from './barrage.js';
 import * as init from './init.js';
 import * as tests from './tests.js';
 import * as provider from './provider.js';
+import * as deck from './deck.js';
 import * as old from './old.js';
---
+++ js/deck.js
@@ -0,0 +1,10 @@
+// @ts-check
+export let carteCriteri = [];
+carteCriteri['1'] = 'A_B_E_1_G_I_J_1A_O_K_L_5_ABC';
+carteCriteri['2'] = 'E_A_C_5_H_J_I_10A_L_K_O_12_CDA';
+carteCriteri['3'] = 'D_A_B_2_I_J_G_5B_K_L_O_9_BCD';
+carteCriteri['4'] = 'E_A_D_5_H_I_J_9A_L_K_O_8_ABC';
+carteCriteri['5'] = 'A_B_C_4_G_J_I_2B_P_K_L_11_DAB';
+carteCriteri['6'] = 'A_B_E_6_J_I_G_3A_N5_K_L_5_DCB';
+carteCriteri['7'] = 'D_A_B_1_I_J_G_5A_K_L_O_8_ABC';
+carteCriteri['8'] = 'A_B_C_9_J_I_G_4B_MP_K_L_12_CBA';
